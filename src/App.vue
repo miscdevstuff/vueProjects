c2e13f6a33e09eade371208f35e7bdd306cf0634
Sohil876 <sohil876@gmail.com>
2023-02-06 16:49:50 +0530

    Move card to its own component file

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
     import q from "./data/quizes.json";
     import { ref, watch } from "vue";
+    import Card from "./components/Card.vue"
     
     const quizes = ref(q);
     const search = ref("");
@@ -17,13 +18,7 @@
                 <input type="text" placeholder="Search..." v-model.trim="search" />
             </header>
         <div class="cards-container">
-            <div class="card" v-for="quiz in quizes" :key="quiz.id">
-                <img alt="" :src="quiz.img" />
-                <div class="card-text">
-                    <h2>{{ quiz.name }}</h2>
-                    <p>{{ quiz.questions.length }} questions</p>
-                </div>
-            </div>
+            <Card v-for="quiz in quizes" :key="quiz.id" :quiz="quiz" />
         </div>
     </div>
 </template>
@@ -53,34 +48,9 @@
         border-radius: 10px;
     }
     
-    /* CARD */
     .cards-container {
         display: flex;
         flex-wrap: wrap;
         margin-top: 40px;
-    }
-    
-    .card {
-        width: 300px;
-        overflow: hidden;
-        border-radius: 2%;
-        box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.1);
-        margin-bottom: 35px;
-        margin-right: 20px;
-        cursor: pointer;
-    }
-    
-    .card img {
-        width: 100%;
-        height: 150px;
-        margin: 0;
-    }
-    
-    .card .card-text {
-        padding: 0 5px;
-    }
-    
-    .card .card-text h2 {
-        font-weight: bold;
     }
 </style>
