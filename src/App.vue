1e14cf3561c3ea0170354e1eb64ff2c4cee0e5d1
Sohil876 <sohil876@gmail.com>
2023-02-17 16:25:48 +0530

    Card style, unimport route and pinia for now

--- src/App.vue
+++ src/App.vue
@@ -11,25 +11,52 @@
 
 <template>
     <main>
-        <div>
-            <h2>Counter</h2>
-            <h1>{{ count }}</h1>
-            <van-space>
-                <van-button icon="plus" type="success" @click="addToCount()" />
-                <van-button icon="minus" type="danger" @click="subtractFromCount()" />
-            </van-space>
+        <div class="container">
+            <div>
+                <h2>Counter</h2>
+                <div>
+                    <h1>{{ count }}</h1>
+                </div>
+                <van-space direction="horizontal">
+                    <van-button icon="minus" type="danger" @click="subtractFromCount()" />
+                    <van-button icon="plus" type="success" @click="addToCount()" />
+                </van-space>
+            </div>
         </div>
     </main>
 </template>
 
 <style scoped>
     main {
-        background-color: aliceblue;
+        display: flex;
+        flex-wrap: wrap;
+        background-color: black;
         width: 100vw;
         height: 100vh;
-        display: flex;
         align-items: center;
         justify-content: center;
+    }
+
+    .container {
+        background-color: rgba(255, 255, 255, 0.1);
+        height: 30%;
+        width: 60%;
+        border-radius: 25px;
+        align-items: center;
+        justify-content: center;
+        margin: 0 auto;
         text-align: center;
+    }
+
+    h1 {
+        color: white;
+    }
+
+    h2 {
+        color: white;
+    }
+
+    h3 {
+        color: white;
     }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+//import { createPinia } from 'pinia'
 // 1. Import the components you need from vant
 import { Button, Space } from 'vant';
 // 2. Import the components style of vant
 import 'vant/lib/index.css';
 
 import App from './App.vue'
-import router from './router'
+//import router from './router'
 
 //import './assets/main.css'
 
@@ -14,7 +14,7 @@ const app = createApp(App)
 
 app.use(Button)
 app.use(Space)
-app.use(createPinia())
-app.use(router)
+//app.use(createPinia())
+//app.use(router)
 
 app.mount('#app')
